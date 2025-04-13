f563cc42ec7bb2a4c6fd83e4171981d505405e2b
Harrison Turpin <harrison.bradley.turpin@gmail.com>
2025-04-13 16:10:45 +0100

    Misdev Adjustments

--- cogs/misdev.py
+++ cogs/misdev.py
@@ -12,45 +12,40 @@ class MissionSubmissionModal(discord.ui.Modal):
         self.mission_pbo = mission_pbo
 
     mission_title = discord.ui.TextInput(style=discord.TextStyle.short, label="Mission Title", required=True, placeholder="Operation Stinky")
-    mission_map = discord.ui.TextInput(style=discord.TextStyle.short, label="Map", required=True, placeholder="Chernarus/Altis/Stratis")
-    mission_uniform = ""
-    mission_size = ""
-    mission_credits = ""
-    #mission_uniform = discord.ui.TextInput(style=discord.TextStyle.short, label="Uniform Camo", required=True, placeholder="MC/MCT/AOR1/MCB")
-    #mission_size = discord.ui.TextInput(style=discord.TextStyle.short, label="Mission Size", required=True, placeholder="Battalion/Squad/Platoon")
-    #mission_credits = discord.ui.TextInput(style=discord.TextStyle.short, label="Credits", required=True, placeholder="")
-
+    # mission_map = discord.ui.TextInput(style=discord.TextStyle.short, label="Map", required=True, placeholder="Chernarus/Altis/Stratis")
     mission_briefing = discord.ui.TextInput(style=discord.TextStyle.short, label="Briefing", required=True, placeholder="https://docs.google.com/document/d/...")
     mission_conceptboard = discord.ui.TextInput(style=discord.TextStyle.short, label="Conceptboard", required=True, placeholder="https://app.conceptboard.com/board/...")
+    mission_notes = discord.ui.TextInput(style=discord.TextStyle.long, label="Notes", required=False, placeholder="What uniform should be used? Any special instructions? Is it battalion or squad sized? Any additional mods required?")
+
 
     async def on_submit(self, interaction: discord.Interaction):
-        self.interaction = interaction
-        self.answer_title = str(self.mission_title) or ""
-        self.answer_map = str(self.mission_map) or ""
-        self.answer_uniform = str(self.mission_uniform) or ""
-        self.answer_size = str(self.mission_size) or ""
-        self.answer_credits = str(self.mission_credits) or ""
-        self.answer_briefing = str(self.mission_credits) or ""
-        self.answer_conceptboard = str(self.mission_conceptboard) or ""
+        answer_title = str(self.mission_title) or ""
+        answer_map = ""
+        answer_briefing = str(self.mission_briefing) or ""
+        answer_conceptboard = str(self.mission_conceptboard) or ""
+        answer_notes = str(self.mission_notes) or ""
 
         #Obtain the map name automatically from the mission file suffix.
-        if self.answer_map == "":
-            self.answer_map = re.search(r"_(.*?)\.pbo", self.answer_title).group(1)
+        if answer_map == "":
+            answer_map = re.search(r"_(.*?)\.pbo", answer_title).group(1)
 
         #Get users to mention, should be the user who submitted the mission and the forum moderators.
         mentions = f"<@{interaction.user.id}>"
         for moderators in self.bot.config['discord']['mission_development']['forum_moderators']:
             mentions += f" <@&{moderators}>"
-        self.answer_post = f"||{mentions}||\n# {self.answer_title}\n**Map:** {self.answer_map}\n**Uniform:** {self.answer_uniform}\n**Size:** {self.answer_size}\n**Credits:** {self.answer_credits}\n**Briefing:** {self.answer_briefing}\n**Conceptboard:** {self.answer_conceptboard}"
+        answer_post = f"||{mentions}||\n# {answer_title}\n**Map:** {answer_map}\n**Briefing:** {answer_briefing}\n**Conceptboard:** {answer_conceptboard}\n\n**Additional Notes:**{answer_notes}"
 
         forum_channel = self.bot.get_channel(int(self.bot.config['discord']['mission_development']['forum_channel_id']))
         tags = [forum_channel.get_tag(int(self.bot.config['discord']['mission_development']['forum_tags']['pending_review']))]
 
+        # Prepare pbo file for upload
+        pbo_file = await self.mission_pbo.to_file(filename=self.mission_pbo.filename, use_cached=False, spoiler=False)
+
         # Create thread
-        self.post = await forum_channel.create_thread(name=f"Mission: {self.answer_title}", applied_tags=tags, content=self.answer_post)
+        post = await forum_channel.create_thread(name=f"Mission: {answer_title}", applied_tags=tags, content=answer_post, file=pbo_file)
 
         # Respond to the interaction
-        await interaction.response.send_message(embed=discord.Embed(description=f"Thanks for your mission submission, misdev will review it and schedule it to be played. You can see the mission submission thread here: <#{self.post[1].id}>", color=0xBEBEFE, ), ephemeral=True)
+        await interaction.response.send_message(embed=discord.Embed(description=f"Thanks for your mission submission, misdev will review it and schedule it to be played. You can see the mission submission thread here: <#{post[1].id}>", color=0xBEBEFE, ), ephemeral=True)
 
         self.stop()
 
